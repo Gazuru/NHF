3e027e9aab07c6607c0c1ae5a69d93d94144b1da
Gazuru <energyteamgazuru@gmail.com>
2019-04-25 10:37:15 +0200

    Kód szépítve

--- tarolo.hpp
+++ tarolo.hpp
@@ -7,11 +7,13 @@ class tarolo {
     size_t meret;
 public:
     tarolo(size_t meret = 0): adat(new T[meret]), meret(meret){}
+
     tarolo(const tarolo& t){
         meret = t.meret;
         for(size_t i = 0; i < meret; i++)
             adat[i] = t.adat[i];
     }
+
     tarolo& operator=(const tarolo& t){
         if(this != &t){
             delete[] adat;
@@ -21,19 +23,23 @@ public:
         }
         return *this;
     }
+
     ~tarolo(){ delete[] adat; }
 
     size_t size() const { return meret; }
+
     T& operator[](size_t i){
         if(i < meret && i >= 0)
             return adat[i];
         throw("Tulindexelve");
     }
+
     T& operator[](size_t i) const{
         if(i < meret && i >= 0)
             return adat[i];
         throw("Tulindexelve");
     }
+
     void push_back(const T& uj){
         T* ujAdat = new T[meret+1];
         for(size_t i = 0; i < meret; i++)
@@ -43,6 +49,7 @@ public:
         adat = ujAdat;
         meret++;
     }
+
     void remove(const T& t){
         for(size_t i = 0; i < meret; i++){
             if(t == adat[i]){
